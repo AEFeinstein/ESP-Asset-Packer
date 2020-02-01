0967047acde015c21c1e4ad3177e4fea2259e540
gelakinetic <gelakinetic@gmail.com>
2020-02-01 11:54:11 -0500

    Asset packer now respects transparency for PNGs

--- pngPacker.py
+++ pngPacker.py
@@ -2,12 +2,51 @@ from PIL import Image
 from espAssetPackerUtils import *
 
 
-def isPixelBlack(pix):
+class pngBytesBuilder:
+    def __init__(self):
+        # Save the name and data
+        self.bytes = bytearray()
+        self.bitIdx = 0
+        self.bitsBeingBuilt = 0
+
+    def appendBit(self, bit):
+        # Only append zeros or ones
+        if(bit != 0):
+            bit = 1
+        # Append the bit
+        self.bitsBeingBuilt = (self.bitsBeingBuilt << 1)
+        self.bitsBeingBuilt = (self.bitsBeingBuilt | bit)
+        # Check if this 32-bit val is finished
+        self.bitIdx = self.bitIdx + 1
+        if(32 == self.bitIdx):
+            # If it is, append it to the output
+            append32bits(self.bytes, self.bitsBeingBuilt)
+            self.bitsBeingBuilt = 0
+            self.bitIdx = 0
+
+    def finish(self):
+        # Pad the last bits out to 32 bytes
+        while(self.bitIdx != 0):
+            self.bitsBeingBuilt = (self.bitsBeingBuilt << 1)
+            self.bitIdx = self.bitIdx + 1
+            if(32 == self.bitIdx):
+                # If it is, append it to the output
+                append32bits(self.bytes, self.bitsBeingBuilt)
+                self.bitsBeingBuilt = 0
+                self.bitIdx = 0
+
+
+def isPixelWhite(pix):
     sumChannel = 0
-    for channel in pix:
-        sumChannel = sumChannel + channel
-    sumChannel = sumChannel / len(pix)
-    return (sumChannel < 128)
+    # RGB come first
+    for channel in range(3):
+        sumChannel = sumChannel + pix[channel]
+    sumChannel = sumChannel / 3
+    return (sumChannel > 128)
+
+
+def isPixelTransparent(pix):
+    return (pix[3] < 128)
 
 
 def processPng(dir, file):
@@ -21,40 +60,35 @@ def processPng(dir, file):
         [type] -- [description]
     """
     # Load the image
-    img = Image.open(dir + "/" + file).convert("RGB")
+    img = Image.open(dir + "/" + file).convert("RGBA")
     pixels = img.load()
 
     # Create a bytearray
-    bytes = bytearray()
+    builder = pngBytesBuilder()
 
     # Append a 32 bit width
-    append32bits(bytes, img.width)
+    append32bits(builder.bytes, img.width)
     # Append a 32 bit height
-    append32bits(bytes, img.height)
+    append32bits(builder.bytes, img.height)
 
-    # Iterate throught the image, packing each pixel into a bit
-    bitsBeingBuilt = 0
-    bitIdx = 0
+   # Iterate throught the image, packing each pixel into a bit
     for x in range(img.height):
         for y in range(img.width):
-
-            if(not isPixelBlack(pixels[y, x])):
-                bitsBeingBuilt = bitsBeingBuilt | (1 << (31-bitIdx))
-
-            # Check if this 32-bit val is finished
-            bitIdx = bitIdx + 1
-            if(32 == bitIdx):
-                # If it is, append it to the output
-                append32bits(bytes, bitsBeingBuilt)
-                bitsBeingBuilt = 0
-                bitIdx = 0
+            pixel = pixels[y, x]
+            if(isPixelTransparent(pixel)):
+                builder.appendBit(0)
+                builder.appendBit(1)
+            elif(isPixelWhite(pixel)):
+                builder.appendBit(0)
+                builder.appendBit(0)
+            else:
+                builder.appendBit(1)
 
     # If there is still a 32-bit val being built, append it
-    if(0 != bitIdx):
-        append32bits(bytes, bitsBeingBuilt)
+    builder.finish()
 
     # Iterating done, close the image
     img.close()
 
     # Return an asset of these bytes
-    return BinaryAsset(file, bytes)
+    return BinaryAsset(file, builder.bytes)
